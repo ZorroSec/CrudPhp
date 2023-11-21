336ae0b422230c27d2bc8c0ce4e7bff1808acb70
ZorroSec <joseiraildesciprianoribeiro@gmail.com>
2023-11-21 14:05:53 -0300

    criando as tables

--- index.php
+++ index.php
@@ -10,20 +10,19 @@
 	<h1 style="text-align: center;">Crud Estudantes</h1>
 	<div class="conteiner">
 		<div class="col-xs-12">
-			<thead>
-				<tr>
-					<th>Nome</th>
-					<th>Idade</th>
-					<th>Acoes</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>Nome</td>
-					<td>Idade</td>
-					<td>Acoes</td>
-				</tr>
-			</tbody>
+			<table class="table">
+				
+				<thead>
+					<tr>
+						<th>Nome</th>
+						<th>Idade</th>
+						<th>Acoes</th>
+					</tr>
+				</thead>
+				<tbody>
+					
+				</tbody>
+			</table>
 		</div>
 	</div>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
